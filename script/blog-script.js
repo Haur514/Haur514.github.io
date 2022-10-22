ced43e18f6fd31e8b35722aa26f8fccce31f8c1a
Haur514 <haruka.yoshioka.514@gmail.com>
2022-10-22 21:25:02 +0900

    タイプスクリプト追加．画像のサイズを動的にリサイズ可能にしました．

---
+++ script/blog-script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+    var targetImg = document.getElementsByClassName("blog-img");
+    console.log(targetImg.length);
+    for (var i = 0; i < targetImg.length; i++) {
+        targetImg[i].style.width = getBlogImgWidth();
+    }
+});
+var getBlogImgWidth = function () {
+    var container = document.getElementById("blog-container");
+    var container_style = window.getComputedStyle(container);
+    return String(parseInt(container_style.width) - 30) + 'px';
+};
