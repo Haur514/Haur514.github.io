4a630aee46d298ec70c478aade7f487cf65704b4
Haur514 <haruka.yoshioka.514@gmail.com>
2022-12-12 14:10:59 +0900

    作品ページを追加しました

--- replace_react/my-page/src/component/WorkComponent.js
+++ replace_react/my-page/src/component/WorkComponent.js
@@ -3,8 +3,16 @@ import "./WorkComponent.css";
 function WorkComponent(props) {
   return (
     <div className="WorkComponent">
-        <div>{props.name}</div>
-        <div>{props.description}</div>
+      <div>{props.name}</div>
+      <img src={props.imgSrc} />
+      <div>{props.description}</div>
+      <div>
+        {props.tags.map((tag) => 
+          <span className="tags">
+            {tag}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
--- replace_react/my-page/src/homepage/Works/Works.js
+++ replace_react/my-page/src/homepage/Works/Works.js
@@ -1,11 +1,7 @@
 import WorkComponent from "../../component/WorkComponent.js";
 import "./Works.css";
-import javaLogo from "./../../images/javalogo.png"
-import cLogo from "./../../images/clogo.png"
-import javascriptLogo from "./../../images/javascriptlogo.png"
-import htmlLogo from "./../../images/HTMLlogo.png"
-import cssLogo from "./../../images/CSSlogo.png"
-import pythonLogo from "./../../images/pythonlogo.png"
+import RatioTimerLogo from "./../../images/RatioTimer.png"
+import JuiceAppLogo from "./../../images/JuiceApp.png"
 
 function Works() {
   return (
@@ -14,8 +10,8 @@ function Works() {
         制作物
       </div>
     <div className="Works">
-      <WorkComponent name="RatioTimer" description="時間が測れる．すごい．"/>
-      <WorkComponent name="JuiceApp" description="ジュースの売買を管理できる．すごい．"/>
+      <WorkComponent name="RatioTimer" description="時間が測れる．すごい．" imgSrc={RatioTimerLogo} tags={["JavaScript","完成済","SaaS"]}/>
+      <WorkComponent name="JuiceApp" description="ジュース販売管理できる．すごい．" imgSrc={JuiceAppLogo} tags={["TypeScript","React","Java Spring Boot","PostgreSQL","In Progress"]}/>
     </div>
     </>
   );
