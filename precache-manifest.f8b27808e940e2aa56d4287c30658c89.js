50b00c54610ac44b90bd06de09dcf91871bc6d9b
Haur514 <haruka.yoshioka.514@gmail.com>
2023-08-04 00:33:17 +0900

    Updates

---
+++ precache-manifest.f8b27808e940e2aa56d4287c30658c89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ad55ba76060e9259794",
+    "url": "/static/css/main.5bcacc16.chunk.css"
+  },
+  {
+    "revision": "7ad55ba76060e9259794",
+    "url": "/static/js/main.7ad55ba7.chunk.js"
+  },
+  {
+    "revision": "62cac4abb9d6098befd4",
+    "url": "/static/js/1.62cac4ab.chunk.js"
+  },
+  {
+    "revision": "b9602c34487e55218d28",
+    "url": "/static/js/2.b9602c34.chunk.js"
+  },
+  {
+    "revision": "6a532cb34d7e88a4030d",
+    "url": "/static/js/runtime~main.6a532cb3.js"
+  },
+  {
+    "revision": "4f5223f7602ed974cd9874eae92607f6",
+    "url": "/static/media/202201_sigss_h-yosiok.4f5223f7.jpg"
+  },
+  {
+    "revision": "bc625c118fd28ebf85459cbeb25a9063",
+    "url": "/static/media/clogo.bc625c11.png"
+  },
+  {
+    "revision": "47d4d83a675b5689f336f17c7285b6d9",
+    "url": "/static/media/javascriptlogo.47d4d83a.png"
+  },
+  {
+    "revision": "d8f88456f524111f1368e90621a2048d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b9efe5f0956901344931607263b8c71.js"
+  "/precache-manifest.f8b27808e940e2aa56d4287c30658c89.js"
 );
 
 workbox.clientsClaim();
