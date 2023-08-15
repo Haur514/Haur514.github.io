5b4cf0bca3831248d3912fd74a1e5d1aca83179e
Haur514 <haruka.yoshioka.514@gmail.com>
2023-08-15 22:34:13 +0900

    Updates

---
+++ precache-manifest.9d8d29eb55a9d0f1d88193d33484e51d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9befaf3769a5f8a28b3",
+    "url": "/static/css/main.e8edc400.chunk.css"
+  },
+  {
+    "revision": "e9befaf3769a5f8a28b3",
+    "url": "/static/js/main.e9befaf3.chunk.js"
+  },
+  {
+    "revision": "dcf916b92703013e6b2e",
+    "url": "/static/js/1.dcf916b9.chunk.js"
+  },
+  {
+    "revision": "057ae36cc56dbe264e69",
+    "url": "/static/js/2.057ae36c.chunk.js"
+  },
+  {
+    "revision": "fe67226b81854493c3e6",
+    "url": "/static/js/runtime~main.fe67226b.js"
+  },
+  {
+    "revision": "4f5223f7602ed974cd9874eae92607f6",
+    "url": "/static/media/202201_sigss_h-yosiok.4f5223f7.jpg"
+  },
+  {
+    "revision": "da69bb2fd957802414de23e68dc562f9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c042ccb3a7a11b76854e5a8e70af0a2.js"
+  "/precache-manifest.9d8d29eb55a9d0f1d88193d33484e51d.js"
 );
 
 workbox.clientsClaim();
