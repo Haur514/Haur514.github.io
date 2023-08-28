74d2df3aebacafb2ed1208219cd33b232a778eea
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2023-08-28 22:15:53 +0900

    Updates

--- precache-manifest.5e68f64404fca002facdfa3ce09086ef.js
+++ precache-manifest.5e68f64404fca002facdfa3ce09086ef.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "e9befaf3769a5f8a28b3",
-    "url": "/static/css/main.e8edc400.chunk.css"
+    "revision": "d3ee32821e2332d64c52",
+    "url": "/static/css/main.725fe102.chunk.css"
   },
   {
-    "revision": "e9befaf3769a5f8a28b3",
-    "url": "/static/js/main.e9befaf3.chunk.js"
+    "revision": "d3ee32821e2332d64c52",
+    "url": "/static/js/main.d3ee3282.chunk.js"
   },
   {
     "revision": "dcf916b92703013e6b2e",
     "url": "/static/js/1.dcf916b9.chunk.js"
   },
   {
-    "revision": "057ae36cc56dbe264e69",
-    "url": "/static/js/2.057ae36c.chunk.js"
+    "revision": "c214f07ec113bfb732a1",
+    "url": "/static/js/2.c214f07e.chunk.js"
   },
   {
     "revision": "fe67226b81854493c3e6",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/static/media/202201_sigss_h-yosiok.4f5223f7.jpg"
   },
   {
-    "revision": "da69bb2fd957802414de23e68dc562f9",
+    "revision": "fc1a8480480c6d0e297df4e486aee263",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d8d29eb55a9d0f1d88193d33484e51d.js"
+  "/precache-manifest.5e68f64404fca002facdfa3ce09086ef.js"
 );
 
 workbox.clientsClaim();
