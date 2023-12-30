3952395e1da706bfad33e9baccdb91c20e50ac9c
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2023-12-30 16:08:38 +0900

    update github actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+require('dotenv').config();
+
 const nextConfig = {
     output: "export",
     productionBrowserSourceMaps: true,
+    assetPrefix: process.env.GITHUB_PAGES ? '/repository-name' : '',
 }
 
 module.exports = nextConfig
