2ec2f275470fddc4300fbc07ef15d52dbc6d01b0
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2023-12-31 13:34:11 +0900

    tiny fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import TabName from '@/components/header/TabName';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -6,6 +7,10 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP" rel="stylesheet"/>
+          <div className="relative bg-gray-900 h-10 text-left p-2">
+            <TabName link="/" tabName="Home"/>
+            <TabName link="/diary" tabName="Diary"/>
+        </div>
         </Head>
         <body>
           <Main />
