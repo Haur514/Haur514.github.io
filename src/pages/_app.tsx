056c632950ea281e15d7f392e2258b97ac2f716d
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2023-12-31 13:16:31 +0900

    delete meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <title>ホームページ</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        {/* <meta name='viewport' content='width=device-width, initial-scale=1' /> */}
         <div className="relative bg-gray-900 h-10 text-left p-2">
           <TabName link="/" tabName="Home"/>
           <TabName link="/diary" tabName="Diary"/>
