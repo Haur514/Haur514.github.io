d49076e40c0c950b683a4509a6b7d9d721856d70
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2024-02-11 22:48:07 +0900

    日記に日付表示させるようにした

--- src/pages/diary/content/index.tsx
+++ src/pages/diary/content/index.tsx
@@ -4,6 +4,17 @@ import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
 
+
+function conv_yyyymmdd(yyyymmdd: string){
+  const str_yyyymmdd = String(yyyymmdd);
+  const ret = str_yyyymmdd.substring(0,4)
+      + "/"
+      + str_yyyymmdd.substring(4,6)
+      + "/"
+      + str_yyyymmdd.substring(6,8);
+  return ret;
+}
+
 function DiaryTextPane(){
   const searchParams = useSearchParams();
   const diary_id = searchParams.get("diary_id")
@@ -21,9 +32,12 @@ function DiaryTextPane(){
   
   return(
     <div className="markdown-preview text-white text-left p-3 m-3 bg-gray-800">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-            {diaryText}
-        </ReactMarkdown>
+      <div className="text-white text-right p-1">
+        {conv_yyyymmdd(diary_id ?? "")}
+      </div>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {diaryText}
+      </ReactMarkdown>
     </div>
   );
 }
